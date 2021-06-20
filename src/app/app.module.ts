3d93955d9ad78c5f406e318cddab628d3317ff1e
Shahzod <48061105+EeciiMcFly@users.noreply.github.com>
2021-06-20 22:01:47 +0500

    Добавил страницу диалогов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from "@angular/router";
-import { RegistrationComponent } from './registration/registration.component'
+import { RegistrationComponent } from './registration/registration.component';
+import { DialogsComponent } from './dialogs/dialogs.component'
 
 const appRoutes: Routes =[
 	{ path: 'registration', component: RegistrationComponent},
 	{ path: 'login', component: LoginComponent},
+	{ path: 'dialogs', component: DialogsComponent}
 ];
 
 @NgModule({
 	imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
-	declarations: [ AppComponent , LoginComponent, RegistrationComponent],
+	declarations: [ AppComponent , LoginComponent, RegistrationComponent, DialogsComponent],
 	bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
